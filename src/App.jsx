e675fced0b199b81c91f714d1f82e43e60abb1a9
rsbisne <roshansb99@gmail.com>
2023-06-04 23:47:57 +0530

    tied message

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react';
 import './styles.scss';
 import Board from './components/Board';
 import { calculateWinner } from './winner';
+import StatusMessage from './components/StatusMessage';
 
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setXNext] = useState(false);
   const winner = calculateWinner(squares);
-  const nextPlayer = isXNext ? 'X' : 'O';
-  const statusMessage = winner
-    ? `Winner is ${winner}`
-    : `Next Player is ${nextPlayer}`;
+
   const handleSquareClick = clickedPosition => {
     if (squares[clickedPosition] || winner) {
       return;
@@ -27,7 +25,7 @@ function App() {
   };
   return (
     <div className="app">
-      <h2>{statusMessage}</h2>
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares} />
       <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+const StatusMessage = ({ winner, isXNext, squares }) => {
+  const noMovesLeft = squares.every(squareValue => squareValue !== null);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const renderStatusMessage = () => {
+    if (winner) {
+      return (
+        <React.Fragment>
+          Winner is {''}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </React.Fragment>
+      );
+    }
+    if (!winner && noMovesLeft) {
+      return (
+        <React.Fragment>
+          <span className="text-orange">O</span> and{' '}
+          <span className="text-green">X</span> tied
+        </React.Fragment>
+      );
+    }
+    if (!winner && !noMovesLeft) {
+      return (
+        <React.Fragment>
+          Next Player is {''}
+          <span className={isXNext ? 'text-green' : 'text-orange'}>
+            {nextPlayer}
+          </span>
+        </React.Fragment>
+      );
+    }
+    return null;
+  };
+
+  return <div className="status-message">{renderStatusMessage()}</div>;
+};
+export default StatusMessage;
