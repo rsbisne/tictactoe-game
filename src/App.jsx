a6d21006fd7fa6a5994cc760ee9438d5589a6bb0
rsbisne <roshansb99@gmail.com>
2023-06-05 20:50:11 +0530

    added font style

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,9 @@ function App() {
   };
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -63,7 +66,7 @@ function App() {
       >
         Start new game
       </button>
-      <h2>Current Game History</h2>
+      <h2 style={{ fontWeight: 'normal' }}>Current Game History</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
